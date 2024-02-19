4bbfee899d6b723d82e12ec7e007560a2af62140
brettg17 <brettgelinas7@gmail.com>
2024-02-19 10:08:47 -0600

    copied connect function from play.js. then exported

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("connected to snek")
+  });
+
+  function handleDisconnection() {
+    console.log('Player has disconnected from the server.');
+    // Additional cleanup or reconnection logic can be added here if needed
+  }
+  
+  
+  conn.on('end', handleDisconnection);
+  conn.on('close', handleDisconnection);
+
+  return conn;
+};
+
+
+console.log("Connecting ...");
+connect();
+
+module.exports = "connect";
