f34390147edb8046c14d846a26d7633da76b355c
brettg17 <brettgelinas7@gmail.com>
2024-02-21 11:17:03 -0600

    added handleUserInput function that currently takes user input to exit game

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
+const { stdin } = require("process");
 const connect = require("./client")
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume()
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === ctrlKey && (key === "c" || key === "C")) {
+    process.exit();
+  }
+}
 connect;
