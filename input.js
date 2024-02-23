7944c38eb47f6fc9682b91820bd15ce086581476
brettg17 <brettgelinas7@gmail.com>
2024-02-22 18:56:19 -0600

    figured out moving but have to figure out how to make it so moving is consistent

--- input.js
+++ input.js
@@ -1,17 +1,44 @@
-const setupInput = function () {
+const { connect } = require("http2");
+
+let connection;
+
+
+const setupInput =  (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume()
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+       process.exit();
+     }
+    if ( key === "w") {
+      conn.write("Move: up")
+    }
+    if ( key === "a") {
+      conn.write( "Move: left")
+    }
+    if ( key === "s") {
+      conn.write( "Move: down")
+    }
+    if ( key === "d") {
+      conn.write( "Move: right")
+    }
+    if ( key === "q") {
+      conn.write( "Say: Snek will devour you") 
+    }
+    if ( key === "e") {
+      conn.write( "Say: get ready to get crushed") 
+    }
+    return
+  }
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  if (key === ctrlKey && (key === "c" || key === "C")) {
-    process.exit();
-  }
-}
 
-module.exports = "setupInput";
+
+module.exports = {setupInput};
+
 
