dcb390f0e89093c6be25b7c03210194170a5f929
brettg17 <brettgelinas7@gmail.com>
2024-02-23 07:48:36 -0600

    added comments for readability

--- client.js
+++ client.js
@@ -12,9 +12,15 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // connect client to the server. when connect succesful displays  connect message to client 
+  //as well as basic instructions on how to play game. display user initials above snake
   conn.on("connect", () => {
     console.log("succesfully connected to gamer server");
     conn.write("Name: BWG");
+    console.log("press 'w' to move up")
+    console.log("press 'a' to move left")
+    console.log("press 's' to move down")
+    console.log("press 'd' to right")
   });
 
   conn.on("data", (data) => {
